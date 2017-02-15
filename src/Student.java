f9b3e128bedeb3f2c39ac910d814941e4c7d40da
elaine <yli2447@uwo.ca>
2017-02-15 17:10:13 -0500

    Added Course class and made changes to instructor and student.

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,21 @@
 /**
  * Created by ethan on 2017-02-07.
  */
+
+import java.util.List;
+
 public class Student extends Person {
     String major;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
+    }
+
+    public void addCourse(Course c){
+
+        courseList.add(c);
     }
 
     public String toString() {
